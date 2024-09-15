025e26a71053f17bee2e8eb62fc442f122bdf97e
Script-eng <lesalonsteven@gmail.com>
2024-09-15 14:33:19 -0700

    adding a .red class to the header

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+// Use jQuery to add a click event handler to the div with ID 'red_header'
+$('#red_header').click(function() {
+  // Add the red class to the header when the div is clicked
+  $('header').addClass('red');
+});
