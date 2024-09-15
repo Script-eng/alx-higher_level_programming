d4063a0cb933ef873848493e9aeddd0d6a8a4e18
Script-eng <lesalonsteven@gmail.com>
2024-09-15 14:30:34 -0700

    using jquery to add class to an element

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Use jQuery to add a click event handler to the div with ID 'red_header'
+$('#red_header').click(function() {
+  // Update the text color of the header to red when the div is clicked
+  $('header').css('color', '#FF0000');
+});
