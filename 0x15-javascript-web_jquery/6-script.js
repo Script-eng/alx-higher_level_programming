d54c43df65702db4ebee2925c97b18a1c85bf2fa
Script-eng <lesalonsteven@gmail.com>
2024-09-15 14:41:45 -0700

    change text on click

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// Add a click event handler to div with ID 'update_header'
+$('#update_header').click(function() {
+  // update the text of the header to 'New Header!!!'
+  $('header').text('New Header!!!');
+});
